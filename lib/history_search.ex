f3cbee52a2341f07a42f3cd92ba787d6471892f9
Matt Mondok <mmondok@salesforce.com>
2016-06-17 22:57:44 -0400

    Change term to command

--- lib/history_search.ex
+++ lib/history_search.ex
@@ -4,17 +4,17 @@ defmodule HistorySearch do
   end
 
   def parse_search(options) do
-    IO.puts "Search for #{options[:term]}"
-    options[:term] |> search
+    IO.puts "Search for #{options[:command]}"
+    options[:command] |> search
   end
 
   def search(nil) do
     IO.puts "Term cannot be nil"
   end
 
-  def search(search_term) do
+  def search(search_command) do
     pid = HistoryDatabase.new(path_to_use)
-    res = HistoryDatabase.search(pid, search_term)
+    res = HistoryDatabase.search(pid, search_command)
     IO.puts StringHelpers.pretty_print(res)
   end
 
@@ -30,7 +30,7 @@ defmodule HistorySearch do
 
   defp parse_args(args) do
     {options, _, _} = OptionParser.parse(args,
-      switches: [term: :string]
+      switches: [command: :string]
     )
     options
   end
